f878c3f8d3d3159ee05d3421096832d287792666
Christina Soulioti <csoulioti@ordereze.com>
2018-10-14 11:50:15 +0300

    Create production Dockerfile

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<p>Byess there!</p>
+					<p>Hi there!</p>
 					<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 						Learn React
 					</a>
